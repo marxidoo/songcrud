9332f05f573e9e592ac0d3f08f78481708734b9e
marxidoo <marxidoo@gmail.com>
2022-10-31 12:21:54 +0100

    my musicapp work

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,21 @@
+ from turtle import title
 from django.db import models
+from datetime import datetime
 
-my new additionCreae your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length = 100)
+    last_name = models.CharField(max_length = 400)
+    age = models.IntegerField(max_length = 20)
+
+class Song(models.Model):
+    Artiste-models.ForeignKey(Artiste, on_delete = models.PROTECT)
+    title = models.CharField(max_length = 100)
+    date_released = models.DateTimeField(max_length = 50)
+    likes = models.CharField(max_length = 120);
+    Artiste_id = models.CharField(max_length = 100)
+    
+class Lyrics(models.Model):
+    Song-models.ForeignKey(Song, on_delete = models.PROTECT)
+    content = models.CharField(max_length = 100)
+    song_id = models.CharField(max_length = 100)
